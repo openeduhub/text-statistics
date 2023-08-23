91d7fd26a17fa90869bd47063c88690c839b461a
joopitz <jonas.opitz@live.de>
2023-08-23 11:58:02 +0200

    feat(nix): validate exported openapi spec

--- flake.nix
+++ flake.nix
@@ -5,7 +5,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    openapi-checks.url = "/home/yusu/work/ITsJointly/projects/openapi-checks";
+    nix-filter.url = "github:numtide/nix-filter";
+    openapi-checks.url = "github:openeduhub/nix-openapi-checks";
     nltk-data = {
       url = "github:nltk/nltk_data";
       flake = false;
@@ -23,6 +24,7 @@
       let
         pkgs = import nixpkgs {inherit system;};
         pkgs-unstable = import nixpkgs-unstable {inherit system;};
+        nix-filter = self.inputs.nix-filter.lib;
         openapi-checks = self.inputs.openapi-checks.lib.${system};
         python = pkgs.python310;
 
@@ -53,7 +55,15 @@
         python-package = python.pkgs.buildPythonPackage rec {
           pname = "text-statistics";
           version = "1.0.4";
-          src = self;
+          src = nix-filter {
+            root = self;
+            include = [
+              "src"
+              ./setup.py
+              ./requirements.txt
+            ];
+            exclude = [ (nix-filter.matchExt "pyc") ];
+          };
           propagatedBuildInputs = (python-packages-build python.pkgs);
           # put nltk-punkt into a directory
           preBuild = ''
@@ -69,6 +79,23 @@
         # but without the importable modules. as a result, it is smaller.
         python-app = python.pkgs.toPythonApplication python-package;
 
+        openapi-schema = pkgs.stdenv.mkDerivation {
+          pname = "${python-app.pname}-openapi-schema";
+          version = python-app.version;
+          src = nix-filter {
+            root = self;
+            include = [ ./src/text_statistics/webservice.py ];
+          };
+          nativeBuildInputs = [
+            pkgs.makeWrapper
+            python-app
+          ];
+          installPhase = ''
+            mkdir $out
+            ${python-app}/bin/openapi-schema | ${pkgs.nodePackages.json}/bin/json > $out/schema.json
+          '';
+        };
+
         # declare how the docker image shall be built
         docker-img = pkgs.dockerTools.buildImage rec {
           name = python-app.pname;
@@ -80,21 +107,22 @@
 
       in {
         packages = rec {
+          inherit openapi-schema;
           text-statistics = python-app;
           docker = docker-img;
           default = text-statistics;
         };
         devShells.default = pkgs.mkShell {
           buildInputs = [
-            (python-packages-devel python.pkgs)
+            (python.withPackages python-packages-devel)
             # python language server
             pkgs.nodePackages.pyright
           ];
         };
         checks = {
           openapi-check = (
-            openapi-checks.openapi-valid {
-              serviceBin = "${self.packages.${system}.text-statistics}/bin/text-statistics";
+            openapi-checks.test-file {
+              openapiFile = "${openapi-schema}/schema.json";
             }
           );
         };
